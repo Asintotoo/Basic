6f10be4214bd30c48452804e20aa15fb9f05c5dc
Asintoto <166311378+Asintotoo@users.noreply.github.com>
2024-06-19 12:36:58 +0200

    Delete src/main/java/com/asintoto/basic directory

--- src/main/java/com/asintoto/basic/enums/ButtonType.java
+++
@@ -1,8 +0,0 @@
-package com.asintoto.basic.enums;
-
-public enum ButtonType {
-    LOCKED,
-    UNLOCKED,
-    CLOSE,
-    PREVIUS
-}
--- src/main/java/com/asintoto/basic/interfaces/BasicSerializable.java
+++
@@ -1,8 +0,0 @@
-package com.asintoto.basic.interfaces;
-
-import java.util.Map;
-
-public interface BasicSerializable {
-
-    public abstract Map<String, Object> serialize();
-}
